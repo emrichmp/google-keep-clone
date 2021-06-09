c0d3fab644d00bb415d5dee8a5d50b6139bdcf3f
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-09 19:51:24 -0400

    added switch state icons

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,11 +2,28 @@ import React from "react";
 import './navbar.css'
 
 class Navbar extends React.Component {
+
+    state = {
+        clicked: false
+    }
+
+    handleClick = () => {
+        this.setState({
+            clicked: !this.state.clicked
+        })
+    }
+
     render(){
         return (
             <div>
                 <nav className="Navbar">
                     <h1 className="Navbar-header"><i className="far fa-lightbulb"></i> Keep</h1>
+                        <input type="text" placeholder="Search"></input>
+                        <i className="fas fa-redo"></i>
+                        <br></br>
+                        <div className="view-icon" onClick={this.handleClick}>
+                            <i className={this.state.clicked ? 'fas fa-grip-horizontal' : 'fas fa-bars'}></i>
+                        </div>
                 </nav>
             </div>
         )
