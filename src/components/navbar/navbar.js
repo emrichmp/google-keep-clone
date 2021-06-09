b6a0b750f1a3bf5c028ee34d3f7d6b6bd5dcd331
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-09 19:33:58 -0400

    added font awesome bulb logo

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,7 +3,11 @@ import React from "react";
 class Navbar extends React.Component {
     render(){
         return (
-            <div>da navbar</div>
+            <div>
+                <nav className="Navbar">
+                    <h1 className="Navbar-header"><i className="far fa-lightbulb"></i> Keep</h1>
+                </nav>
+            </div>
         )
     }
 }
