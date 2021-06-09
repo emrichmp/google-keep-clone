873ea1d7fef78b0785dcf37e4bd285c298de8419
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-09 19:29:13 -0400

    added components folder, navbar folder and navbar component

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    render(){
+        return (
+            <div>da navbar</div>
+        )
+    }
+}
+
+export default Navbar;
