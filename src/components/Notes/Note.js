c0a2b5ab3fb58dbd367ac9367bfa45c15dd300db
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-10 22:05:13 -0400

    note

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+//This component will be the individual notes
+
 class Note extends React.Component{
     render(){
         return(
-            <div></div>
+            <div>I am a note</div>
         )
     }
 }
